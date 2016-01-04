9d520115c44b3295118a0b59a943a93a7434815e
ildaribragimov <iibragimov84@gmail.com>
2016-01-05 00:17:08 +0300

    1) Вынесение обработчика во внешнюю функцию 2) Создание минимизированной версии кода обработчика

--- www/Demos/emulateMouseEnterLeave/script.js
+++ www/Demos/emulateMouseEnterLeave/script.js
@@ -27,40 +27,42 @@ document.addEventListener("DOMContentLoaded", function() {
         cursorOn = null; // Запоминаем, что по умолчанию курсор не над анимируемым элементом
     
     section.onmouseover = section.onmouseout = function(event){
-        // Объявляем переменные:
+
         debag.innerHTML += cursorOn+'\n';
-        
+
+        // Объявляем переменные:
         var type = event.type, // Тип события
-            target = event.target, // Элемент, на котором произошло событие
-            relatedTarget = event.relatedTarget, // Исходный элемент до возникновения события
-            targetEl = (type == 'mouseover')? event.target: event.relatedTarget, // Элемент, над которым сейчас курсор
-            relatedTargetEl = (type == 'mouseover')? event.relatedTarget: event.target, // Элемент, с которого перешла мышь
-            action = (type == 'mouseover')? 'show': 'hide', // Анимационное действие (в зависимости от события)
-            animatedElCssClass = '.'+animatedElClass.replace(' ', '.');
-        
-        if(targetEl && relatedTargetEl){
-        
-        // Вносим информацию в блок отладчика
-        debag.innerHTML += '"'+type+'" на '+target.tagName+'.'+target.className+'\n';
-        debag.innerHTML += 'Перешли с '+relatedTargetEl.tagName+'.'+relatedTargetEl.className+' на '+targetEl.tagName+'.'+targetEl.className+'\n';
-        }
-        
+            target = event.target; // Элемент, на котором произошло событие
+
         // Выходим из обработчика, если:
         // - событие произошло на элементе, которому был назначен обработчик
         // - если переходы происходят внутри анимируемого элемента
         if( target == this || (type == 'mouseout' && !cursorOn) || (type == 'mouseover' && cursorOn) ){
             debag.innerHTML += 'Выходим из обработчика!\n------------------------\n';
             return;
-        }        
-        
+        }
+
+        var relatedTarget = event.relatedTarget, // Исходный элемент до возникновения события
+            targetEl = (type == 'mouseover')? target: relatedTarget, // Элемент, над которым сейчас курсор
+            relatedTargetEl = (type == 'mouseover')? relatedTarget: target, // Элемент, с которого перешла мышь
+            action = (type == 'mouseover')? 'show': 'hide', // Анимационное действие (в зависимости от события)
+            animatedElCssClass = '.'+animatedElClass.replace(' ', '.');
+
+        if(targetEl && relatedTargetEl){
+            // Вносим информацию в блок отладчика
+            debag.innerHTML += '"'+type+'" на '+target.tagName+'.'+target.className+'\n';
+            debag.innerHTML += 'Перешли с '+relatedTargetEl.tagName+'.'+relatedTargetEl.className+' на '+targetEl.tagName+'.'+targetEl.className+'\n';
+        }
+
         if(!cursorOn){
+            var parent = targetEl.parent(animatedElCssClass);
             // Вносим информацию в блок отладчика
             debag.innerHTML += 'Курсор перешел на анимируемый элемент!\n';
             if(targetEl.classList.contains('animated')){
                 // Запоминаем, что курсор перешел на анимируемый элемент
                 cursorOn = targetEl;
-            }else if(targetEl.parent(animatedElCssClass)){
-                cursorOn = targetEl.parent(animatedElCssClass);
+            }else if(parent){
+                cursorOn = parent;
             }
             // Изменяем CSS-свойсва анимируемого элемента
             animate(cursorOn, action);
@@ -70,7 +72,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
         
         // Выходим из обработчика, если переход был внутри анимируемого элемента
-        if( targetEl && targetEl.parent(animatedElCssClass) == cursorOn ){
+        if( targetEl && (parent == cursorOn) ){
             debag.innerHTML += 'Это внутренний переход!\nВыходим из обработчика!\n------------------------\n';
             return;
         }
