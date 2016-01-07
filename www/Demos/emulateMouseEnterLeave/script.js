e7210dba062c69b2d489df459a46f40164281bb7
ildaribragimov <iibragimov84@gmail.com>
2016-01-08 02:21:53 +0300

    Полное завершение разработки решения ЭМУЛЯЦИЯ СОБЫТИЙ MOUSEENTER И MOUSELEAVE

--- www/Demos/emulateMouseEnterLeave/script.js
+++ www/Demos/emulateMouseEnterLeave/script.js
@@ -3,88 +3,74 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
-
-   var debag = document.querySelector('textarea');
-
+    var debag = document.querySelector('textarea');
+    
     // 
     function animate(el, action){
         switch(action){
             case 'show':
-                debag.innerHTML += 'Назначаем ему новые стили!\n';
+                debag.innerHTML += 'Назначаем новые стили!\n';
                 el.style.backgroundColor = 'pink';
                 break;
             case 'hide':
-                debag.innerHTML += 'Сбрасываем его стили!\n';
+                debag.innerHTML += 'Сбрасываем стили!\n';
                 el.style.backgroundColor = '';
                 break;
         }
     }
  
     
+    function emulateMOverMOut(event){
+        // Объявляем переменные:
+        var target = event.target; // Элемент, на котором произошло событие
+        // Выходим из обработчика, если событие произошло на элементе, которому был назначен обработчик
+        if(target == this){
+            // Вносим информацию в блок отладчика
+            debag.innerHTML += 'Событие произошло на элементе, которому был назначен обработчик!\nВыходим из обработчика!\n------------------------\n';
+            return;
+        }
+        // Объявляем переменные:
+        var type = event.type, // Тип события
+            relatedTarget = event.relatedTarget, // Исходный элемент до возникновения события
+            animatedElCssClass = '.'+animatedElClass.replace(' ', '.'), // Замена пробелов на точки в строке значения атрибута "class" для корректного поиска родеителей по CSS-селектору
+            targetParent = target.parent(animatedElCssClass), // Ссылка на родителя с искомым CSS-классом
+            relatedTargetParent = relatedTarget.parent(animatedElCssClass); // Ссылка на родителя с искомым CSS-классом
+        // Обрабатываем события на потомках, если хотя бы у одного из элементов был найден искомый родитель
+        if( targetParent || relatedTargetParent ){
+            // Костли для свойств объекта EVENT, если элемент - тег <a>
+            target = (target.tagName == 'A') ? toString.call(target): target;
+            relatedTarget = (relatedTarget.tagName == 'A') ? toString.call(relatedTarget): relatedTarget;
+            // Выходим из обработчика, если переход был совершен в пределах одного анимируемого элемента
+            if( targetParent == relatedTargetParent || target == relatedTargetParent || relatedTarget == targetParent ){
+                // Вносим информацию в блок отладчика
+                debag.innerHTML += 'Переход внутри анимируемого элемента\n------------------------\n';
+                return;                                          
+            }
+            // Вносим информацию в блок отладчика
+            debag.innerHTML += 'Прыжок на потомка анимируемого элемента\nЕго родителю ';
+            // Переопределяем целевой элемент, если он - сам анимируемый элемент 
+            target = targetParent || target;
+        }
+        // Если элемент является анимируемым
+        if( target.classList.contains(animatedElClass) ){
+            // Объявляем переменные:
+            var action = (type == 'mouseover')? 'show': 'hide'; // Анимационное действие (в зависимости от события)
+            // Переопределяем значение переменной
+            cursorOn = ( cursorOn == null || cursorOn != target ) ? target: null;
+            // Изменяем CSS-свойсва анимируемого элемента
+            animate(target, action);
+            // Вносим информацию в блок отладчика
+            debag.innerHTML += '------------------------\n';
+        }
+    }
+
+
+    
     // Получение ссылки на объект, которому необходимо назначить обработчик
     var section = document.querySelector('section'), // Получаем ссылку на элемент, которому будем назначать обработчики событий "mouseover" и "mouseout"
         animatedElClass = 'animated', // Указываем имя CSS-класса анимируемого элемента
         cursorOn = null; // Запоминаем, что по умолчанию курсор не над анимируемым элементом
+
+    section.onmouseover = section.onmouseout = emulateMOverMOut;
     
-    section.onmouseover = section.onmouseout = function(event){
-
-        debag.innerHTML += cursorOn+'\n';
-
-        // Объявляем переменные:
-        var type = event.type, // Тип события
-            target = event.target; // Элемент, на котором произошло событие
-
-        // Выходим из обработчика, если:
-        // - событие произошло на элементе, которому был назначен обработчик
-        // - если переходы происходят внутри анимируемого элемента
-        if( target == this || (type == 'mouseout' && !cursorOn) || (type == 'mouseover' && cursorOn) ){
-            debag.innerHTML += 'Выходим из обработчика!\n------------------------\n';
-            return;
-        }
-
-        var relatedTarget = event.relatedTarget, // Исходный элемент до возникновения события
-            targetEl = (type == 'mouseover')? target: relatedTarget, // Элемент, над которым сейчас курсор
-            relatedTargetEl = (type == 'mouseover')? relatedTarget: target, // Элемент, с которого перешла мышь
-            action = (type == 'mouseover')? 'show': 'hide', // Анимационное действие (в зависимости от события)
-            animatedElCssClass = '.'+animatedElClass.replace(' ', '.');
-
-        if(targetEl && relatedTargetEl){
-            // Вносим информацию в блок отладчика
-            debag.innerHTML += '"'+type+'" на '+target.tagName+'.'+target.className+'\n';
-            debag.innerHTML += 'Перешли с '+relatedTargetEl.tagName+'.'+relatedTargetEl.className+' на '+targetEl.tagName+'.'+targetEl.className+'\n';
-        }
-
-        if(!cursorOn){
-            var parent = targetEl.parent(animatedElCssClass);
-            // Вносим информацию в блок отладчика
-            debag.innerHTML += 'Курсор перешел на анимируемый элемент!\n';
-            if(targetEl.classList.contains('animated')){
-                // Запоминаем, что курсор перешел на анимируемый элемент
-                cursorOn = targetEl;
-            }else if(parent){
-                cursorOn = parent;
-            }
-            // Изменяем CSS-свойсва анимируемого элемента
-            animate(cursorOn, action);
-            // Вносим информацию в блок отладчика
-            debag.innerHTML += '------------------------\n';
-            return;
-        }
-        
-        // Выходим из обработчика, если переход был внутри анимируемого элемента
-        if( targetEl && (parent == cursorOn) ){
-            debag.innerHTML += 'Это внутренний переход!\nВыходим из обработчика!\n------------------------\n';
-            return;
-        }
-        
-        // Вносим информацию в блок отладчика
-        debag.innerHTML += 'Курсор вышел из анимируемого элемента!\n';
-        // Изменяем CSS-свойсва анимируемого элемента
-        animate(cursorOn, action);
-        // Запоминаем, что курсор перешел на анимируемый элемент
-        cursorOn = null;
-        // Вносим информацию в блок отладчика
-        debag.innerHTML += '------------------------\n';
-    }
-
 });
