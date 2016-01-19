c44c75fd4c434ad0951dc4880d75717d1a9b3ef6
ildaribragimov <iibragimov84@gmail.com>
2016-01-19 08:23:48 +0300

    Реализована передача CSS-свойств, которые необходимо изменить в процессе анимации в полифилл. Реализован перебор  объекта CSS-свойств с фильтрацией (только свобственные свойства)

--- www/Demos/animate.polyfill/script.js
+++ www/Demos/animate.polyfill/script.js
@@ -8,13 +8,21 @@
  * Аргументы:
  * * properties (тип: object) - Объект целевых значений CSS-свойств, которые должны быть изменены
  * * callback (тип: function) - Пользовательская функция обратного вызова, которая исполняется, по завершении анимации
- * * easingFunction (тип: string) - Функция динамики выполнения, описывающая график анимации.
- * * duration (тип: number) - Длительность анимации
- * * fps (тип: number) - Скорость скены кадров в секунду
+ * * options (тип: object) - Объект параметров анимации. Содержит слудующие параметры:
+ * * * easingFunction (тип: string) - Функция динамики выполнения, описывающая график анимации.
+ * * * duration (тип: number) - Длительность анимации
+ * * * fps (тип: number) - Скорость скены кадров в секунду
  */
-Object.prototype.animate = function(properties, callback = null, easingFunction = 'linear', duration = 1000, fps = 50){
-    // Здесь выполняется сама анимация
-    this.style.width = "350px";
+
+Object.prototype.animate = function(properties, callback = null, options = {easingFunc:'linear', duration:1000, fps:50}){
+    // Перебор свойств объекта в цикле
+    for(var property in properties){
+        // Если текущее свойство является собственным (не унаследованным)
+        if (properties.hasOwnProperty(property)) {
+            // Вывод пары "ключ => значение" в консоль браузера
+            console.log( "Ключ: " + property + " значение: " + properties[property] );
+        }
+    }
     // Вызов пользовательской функции после окончания анимации
     if ( callback ) { callback(); }
 }
@@ -26,7 +34,7 @@ document.querySelector('a')
         // Отмена действия по умолчанию браузера на событие
         event.preventDefault();
         // вызов метода анимации на целевом объекте
-        event.target.animate({width:'350px'}, function(){
+        event.target.animate({width:'350px', height:'400px'}, function(){
             console.log('Анимация завершена!');
         });
     });
