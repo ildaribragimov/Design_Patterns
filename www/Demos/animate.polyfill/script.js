d1fb10ad75f2ee5b38274852d35cc7ac508abdee
Ildar Ibragimov <iibragimov84@gmail.com>
2016-01-22 20:44:39 +0300

    Завершение разработки оптимизированной версии полифилла метода "animate", расширяющего существующие методы объектов DOM Javascript

--- www/Demos/animate.polyfill/script.js
+++ www/Demos/animate.polyfill/script.js
@@ -10,69 +10,73 @@
  * * callback (тип: function) - Пользовательская функция обратного вызова, которая исполняется, по завершении анимации
  * * options (тип: object) - Объект параметров анимации. Содержит слудующие параметры:
  * * * easingFunction (тип: string) - Функция динамики выполнения, описывающая график анимации.
- * * * duration (тип: number) - Длительность анимации
- * * * fps (тип: number) - Скорость скены кадров в секунду
+ * * * duration (тип: number) - Продолжительность анимации
  */
-Object.prototype.animate = function(properties, callback = null, options = {easingFunc, duration, fps}){
+Object.prototype.animate = function(properties, callback = null, options = {easingFunc, duration}){
     // Назначение значений по умолчанию параметрам анимации, если они не были переданы в вызове
     options.easingFunc = options.easingFunc || 'linear';
     options.duration = options.duration || 1000;
-    options.fps = options.fps || 50;
-    
     // Объявление перменных
     var self = this, // Сохранение ссылки на элемент на котором был вызван метод в переменную
-        interval = 1000/options.fps, // Интервал анимации в миллисекундах
-        animationSteps = Math.ceil( options.duration/interval ), // Количество кадров анимации, за указанное время
         elementStyle = getComputedStyle(self), // Получение исходных CSS-стилей элемента, на котором вызван метод анимации
-        startingPointStyle = new Object, // 
+        startingPointStyle = new Object, //
         deltaStylesPerStep = new Object; // Создание объекта значнений на которые должны измениться CSS-свойства за один кадр анимации
-
     // Перебор анимирумых свойств объекта в цикле
     for(var property in properties){
         // Если текущее свойство является собственным (не унаследованным)
         if (properties.hasOwnProperty(property)) {
             // 
             startingPointStyle[property] = parseFloat( elementStyle[property] );
-            // Получение значнений на которые должны измениться CSS-свойства за один кадр и добавние их в массив
-            deltaStylesPerStep[property] = (parseFloat( properties[property] ) - startingPointStyle[property]) / animationSteps;
+            // Создание объекта значнений на которые должны изменяться CSS-свойства за 1мс времени по линейной функции
+            deltaStylesPerStep[property] = (parseFloat( properties[property] ) - startingPointStyle[property]) / options.duration;
         }
     }
-
-    // Назначение начального номера шага
-    var stepNumber = 1;
-    // Объявление интервальной функции
-    var animate = setInterval( function(){
-        // Если истекло указанное время анимации
-        if(stepNumber <= animationSteps){
-            // Выполнение анимации по пользовательской функции анимации
-            switch (options.easingFunc){
-                // Если функция - линейная
-                case 'linear':
-                    // Обход объекта значений, на которые должны измениться стили за один шаг
-                    for(var prop in deltaStylesPerStep){
-                        // Если текущее свойство является собственным (не унаследованным)
-                        if (deltaStylesPerStep.hasOwnProperty(prop)) {
-                            // Изменение значений CSS-свойств элемента
-                            self.style[prop] = (startingPointStyle[prop] + stepNumber * deltaStylesPerStep[prop])+"px";
-                        }
+    // Метод анимации в момент timePassed по заданной функции динамики
+    function draw(easingFunc, timePassed) {
+        // Выполнение анимации по пользовательской функции анимации
+        switch (options.easingFunc){
+            case 'linear':
+                // Перебор анимирумых свойств объекта в цикле
+                for(var property in properties){
+                    // Если текущее свойство является собственным (не унаследованным)
+                    if (properties.hasOwnProperty(property)) {
+                        // Изменение значений CSS-свойств элемента
+                        self.style[property] = startingPointStyle[property] + timePassed * deltaStylesPerStep[property]+"px";
                     }
-                    // Прерывание выполнения и выход из конструкции SWITCH
-                    break;
-                // Если функция пользовательская
-                default:
-                    options.easingFunc();
-                    // Прерывание выполнения и выход из конструкции SWITCH
-                    break;
-            }
-            // Увеличиваем счетчик шагов на единницу
-            stepNumber++;
-        }else{
-            // Остановка выполнения интервальной функции
-            clearInterval(animate);
-            // Вызов пользовательской функции обратного вызова после окончания анимации
-            if(callback){ callback(); }
+                }   
+                // Прерывание выполнения и выход из конструкции SWITCH
+                break;
+            // Если функция пользовательская
+            default:
+                options.easingFunc();
+                // Прерывание выполнения и выход из конструкции SWITCH
+                break;
         }
-    }, interval );
+    }
+    // Метод с рекурсивным вызовом самого себя в зависимости от тех.возможностей браузера
+    function animate(time){
+        // Объявление переменных:
+        var timePassed = time - animationStartTime; // Время, прошедшее с начала анимации
+        // Если произошло превышение времени анимации, тогда происходит фиксация конца анимации
+        if (timePassed > options.duration) {
+            // Присвоение переменной прошедшего времени значения переменной продолжительности анимации
+            timePassed = options.duration;
+        }
+        // Вызов метода анимации в момент timePassed
+        draw(options.easingFunc, timePassed);
+        // Если время анимации не истекло
+        if (timePassed < options.duration) {
+            // Повторный вызов функции (следующего шага) анимации
+            requestAnimationFrame(animate);
+            // Прерывание выполнения и выход из функции
+            return;
+        }
+        // Вызов пользовательской функции обратного вызова после окончания анимации, если пользовательская функция задана
+        if(callback){ callback(); }            
+    }
+    // Объявление перменных:
+    var animationStartTime = performance.now(), // Время, прошедшее с начала загрузки страницы.
+        animationStep = requestAnimationFrame(animate); // Идентификатор шага анимации, возвращенный функцией "requestAnimationFrame"
 }
 
 // Получение ссылки на объект с именем HTML-тега "а"
@@ -84,5 +88,5 @@ document.querySelector('span')
         // вызов метода анимации на целевом объекте
         event.target.animate({width:"350px", height:"200px"}, function(){
             console.log('Анимация завершена!');
-        }, {duration:50});
+        }, {duration:100});
     });
