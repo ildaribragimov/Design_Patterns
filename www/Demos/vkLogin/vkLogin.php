dc4c77b5a4e3a96a1118b3267c41a408db4e4dee
Ildar Ibragimov <iibragimov84@gmail.com>
2016-03-21 14:57:36 +0300

    Корректировка файлов демонстрационной версии решения vkLogin

--- www/Demos/vkLogin/vkLogin.php
+++ www/Demos/vkLogin/vkLogin.php
@@ -2,7 +2,11 @@
 /**
  * Файл-контроллер
  */
-
+// Подключение вспомогательного файла PHP-класса работы с пользователем
+include_once "../../library/php/VK/VK.php";
+// 
+$vk = new VK(5218692);
+echo "<div><br>".$vk->getProperty("app_id")."<br></div>";
 // Если массив переменных HTTP-запроса пуст
 if (empty($_REQUEST)) {
     // Загрузка шаблона вывода формы авторизации
@@ -10,10 +14,13 @@ if (empty($_REQUEST)) {
     // Прерывание выполнение скрипта
     return;
 }
+
 // Подключение вспомогательного файла PHP-класса работы с пользователем
-include_once "helpers/user.php";
-//
-$userInfo = getUserInfo($_REQUEST["mid"], "id,first_name,last_name,photo_200_orig,domain");
+include_once "../../library/php/VK/VK.php";
+// 
+$vk = new VK(5218692);
+
+//$userInfo = getUserInfo($_REQUEST["mid"], "id,first_name,last_name,photo_200_orig,domain");
 
 // Подключение шаблона вывода формы добавления отзыва
 include_once "template/sendReview.php";
