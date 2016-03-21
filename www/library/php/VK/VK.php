0c8da8fdfb95e5112cdc74a152b44ba386e25d1d
Ildar Ibragimov <iibragimov84@gmail.com>
2016-03-21 16:02:10 +0300

    Внесение корректировок в код php-класса VK: 1) Указание правильного пути к файлу базового php-класса Object 2) Наследование класса Object классом VK 3) Удаление переопределяющего метода setProperty у класса Vk

--- www/library/php/VK/VK.php
+++ www/library/php/VK/VK.php
@@ -1,4 +1,6 @@
 <?php
+//
+include_once $_SERVER['DOCUMENT_ROOT']."/library/php/Object/Object.php";
 /**
  * Класс работы с API ВКонтакте
  *
@@ -12,9 +14,8 @@
  * @property number $api_url - Базовый путь для совершения запросов к API ВКонтакте
  *
  * @method void __construct(number $app_id, string $api_secret, string $api_url) Конструктор Класса "VK"
- * @method void setProperty(string $name, string $value) Переопределяет значение свойству объекта
  */
-class VK
+class VK extends Object
 {
     protected $app_id      = null;
     protected $api_secret  = null;
@@ -29,23 +30,11 @@ class VK
      *
      * @return void
      */
-    function __construct ($app_id, $api_secret, $api_url = null)
+    function __construct ($app_id, $api_secret = null, $api_url = null)
     {
         $this->app_id      = $app_id;
         $this->api_secret  = $api_secret;
         $this->api_url     = $api_url ? $api_url : $this->api_url;
-    }
-    /**
-     * Метод переопределяет значение свойства объекта
-     *
-     * @param string $name - Имя переопределяемого свойства объекта
-     * @param string $value - Значение свойства объекта
-     *
-     * @return void
-     */
-    protected function setProperty($name, $value)
-    {
-        $this->$name = $value;
     }
 }
 ?>
