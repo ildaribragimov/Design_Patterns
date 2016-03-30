210dd05090b080581f6b67b53218c18296e906e6
Ildar Ibragimov <iibragimov84@gmail.com>
2016-03-30 20:57:35 +0300

    тестирование добавленных методов динамического создания объектов и получения информации о пользователях ВКонтакте

--- www/Demos/vkLogin/vkLogin.php
+++ www/Demos/vkLogin/vkLogin.php
@@ -7,6 +7,14 @@ include_once $_SERVER['DOCUMENT_ROOT']."/library/php/VK/VK.php";
 // 
 $vk = new VK(5218692);
 echo "<div><br>".$vk->getProperty("app_id")."<br></div>";
+$vkUsers = $vk->getUsers();
+//$U = $vkUsers->get("ildar_ibragimov", "id,first_name,last_name,photo_200_orig,domain", "gen");
+$U = $vkUsers->get("13654436,2168044");
+
+echo "<pre>";
+print_r($U);
+echo "</pre>";
+
 // Если массив переменных HTTP-запроса пуст
 if (empty($_REQUEST)) {
     // Загрузка шаблона вывода формы авторизации
@@ -16,9 +24,9 @@ if (empty($_REQUEST)) {
 }
 
 // Подключение вспомогательного файла PHP-класса работы с пользователем
-include_once "../../library/php/VK/VK.php";
+//include_once "../../library/php/VK/VK.php";
 // 
-$vk = new VK(5218692);
+//$vk = new VK(5218692);
 
 //$userInfo = getUserInfo($_REQUEST["mid"], "id,first_name,last_name,photo_200_orig,domain");
 
