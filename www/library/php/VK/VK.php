c6c475815eb4dcf0888f3321b2d2f049959d1bab
Ildar Ibragimov <iibragimov84@gmail.com>
2016-04-01 17:04:39 +0300

    Добавление метода request класса vk

--- www/library/php/VK/VK.php
+++ www/library/php/VK/VK.php
@@ -14,6 +14,7 @@ include_once $_SERVER['DOCUMENT_ROOT']."/library/php/object/object.php";
  * @property number $api_url - Базовый путь для совершения запросов к API ВКонтакте
  *
  * @method void __construct(number $app_id, string $api_secret, string $api_url) Конструктор Класса "VK"
+ * @method object request(string $method, object $params) Выполняет запрос к APi ВКонтакте и возвращает его результат
  * @method object getUsers() Получает экземпляр класса "VKUsers" 
  */
 class vk extends Object
@@ -38,13 +39,40 @@ class vk extends Object
         $this->api_url     = $api_url ? $api_url : $this->api_url;
     }
     /**
+     * Метод выполняет запрос к APi ВКонтакте и возвращает его результат
+     *
+     * @param string $method - Имя метода API ВКонтакте, который будет вызван
+     * @param object $params - Список параметров, вызываемого метода
+     *
+     * @return object $result - Результат запроса к API ВКонтакте
+     */
+    public function request ($method, $params = null)
+    {
+        // Формирование URI-строки параметров метода
+        if ($params) {
+            $counter = 0;
+            foreach ($params as $key => $value) {
+                $paramsStr .= ($counter == 0) ? "?" : "&";
+                $paramsStr .= $key.'='.$value;
+                $counter++;
+            }
+        }
+        // Формирование строки запроса к API ВКонтакте
+        $query = $this->api_url
+               . $method
+               . $paramsStr;
+        // Получение результата запроса в виде объекта
+        $result = json_decode(file_get_contents($query));
+        return $result;
+    }
+    /**
      * Метод получает создает экземпляр класса работы с пользователями и возвращает ссылку на этот экземпляр
      *
      * return object $vkUsers - Экземпляр объекта "VKUsers"
      */
     public function getUsers ()
     {
-        return $this->loadObject(__DIR__, "vkUsers");
+        return $this->loadObject(__DIR__, "vkUsers", $this);
     }
 }
 ?>
