01c98e4ebaecf8d3e73d14807f276ad34e629a41
Ildar Ibragimov <iibragimov84@gmail.com>
2016-04-02 16:18:52 +0300

    Создание HTML-разметки шаблона блока выторизации через ВКонтакте

--- www/Demos/vkLogin/template/signIn.php
+++ www/Demos/vkLogin/template/signIn.php
@@ -5,7 +5,25 @@
 ?>
 <script defer type="text/javascript">
     "use strict";
-    alert("sdff");
+    document.querySelector(".signInThroughVk").addEventListener("click", function(e){
+        e.preventDefault();
+
+        VK.Auth.login(function(response) { 
+            console.log(response.session);
+            console.log(response.settings);
+            
+            if (response.session) {
+                /* Пользователь успешно авторизовался */ 
+                if (response.settings) {
+                    /* Выбранные настройки доступа пользователя, если они были запрошены */ 
+                } 
+            } else {
+                /* Пользователь нажал кнопку Отмена в окне авторизации */         
+            } 
+        }); 
+
+        return false;
+    });
 </script>
 <div>
     <div><img src="" alt=""></div>
