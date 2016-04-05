791dd80c61a9033a5d9ae25d9d65863a9eb1706e
Ildar Ibragimov <iibragimov84@gmail.com>
2016-04-05 19:37:57 +0300

    1) Добавление описательных комментариев в соответствии со спецификацией JSDoc 2) Оптимизация кода.

--- www/library/js/Ajax/ajax.js
+++ www/library/js/Ajax/ajax.js
@@ -1,17 +1,20 @@
 "use strict";
-
 /**
- * Объект "Всплывающее окно"
+ * Класс XMLHttpRequest-запроса
  *
- * @constructor
- * @this {Ajax}
+ * @class
+ * @classdesc Выполняет XMLHttpRequest-запрос и возвращает в функциях обратного вызова результат. Принимает в параметрах все необъодимые данные для осуществления XMLHttpRequest-запроса.
+ *
  * @param {object} settings - Объект параметров запроса XMLHttpRequest
  * @param {string} settings.url - Адрес запроса
- * @param {string} settings.data - Данные, отправляемые на сервер. Могут быть переданы только как URI-строка или javascript-объекта
- * @param {string} settings.method - Метод отправки запроса
- * @param {boolean} settings.async - Тип запроса. "true" - асинхронный, "false" - синхронный
- * @param {function} settings.error - Функция, которая будет вызвана в случае неудачного завершения запроса к серверу
- * @param {function} settings.success - Функция, которая будет вызвана в случае удачного завершения запроса к серверу
+ * @param {string} [settings.data = null] - Данные, отправляемые на сервер. Могут быть переданы только как URI-строка или javascript-объекта
+ * @param {string} [settings.method = get] - Метод отправки запроса
+ * @param {boolean} [settings.async = true] - Тип запроса: асинхронный ("true"), синхронный ("false")
+ * @param {requestCallback} [settings.error = null] - Функция, которая будет вызвана в случае неудачного завершения запроса к серверу
+ * @param {requestCallback} [settings.success = null] - Функция, которая будет вызвана в случае удачного завершения запроса к серверу
+ *
+ * @author Ildar Ibragimov <iibragimov84@gmail.com>
+ * @copyright Ildar Ibragimov 2016
  */
 function ajax(settings) {
     // Назначение значений по умолчанию параметрам объекта окна, если они не были переданы в вызове
@@ -20,30 +23,33 @@ function ajax(settings) {
     settings.async = settings.async || "true";
     settings.error = settings.error || null;
     settings.success = settings.success || null;
+
     /**
      * Метод формирует URI-строку запроса
      *
      * @private
-     * @param {object} obj - Формирует URI-строку запроса из javascript-объекта
+     * @param {object} obj - Параметры в виде объекта, которые необходимо преобразовать в URI-строку
      * @return {string} str - Сформированная URI-строка
+     *
+     * @description Формирует URI-строку запроса из javascript-объекта параметров, полученных при создании экземпляра объекта AJAX.
      */
     function getUriParams(obj) {
-        // Определение перменной URI-строки
         var str = "";
-        // Перебор эелементов передаваемого объекта данных в цикле
         for (var param in obj) {
-            // Если текущее свойство является собственным (не унаследованным)
+            // Добавление подстроки "параметр=значение" к основной URI-строке, если текущее свойство - собственное (не унаследованное)
             if (obj.hasOwnProperty(param)) {
-                str = str + param + "=" + encodeURIComponent(obj[param]) + "&";
+                str += param + "=" + encodeURIComponent(obj[param]) + "&";
             }
         }
         return str.substring(0, str.length - 1);
     }
+
     // Определение значения по умолчанию тела запроса
     var body = null;
+
     // Если объект передаваемых данных не пуст
     if (settings.data) {
-        // Преобразование объекта, отправляемых данных в URI-строку
+        // Преобразование объекта данных в URI-строку, если объект отправляемых данных не пуст
         if (typeof settings.data === 'object') {
             settings.data = getUriParams(settings.data);
         }
@@ -57,17 +63,17 @@ function ajax(settings) {
                 break;
         }
     }
+
     // Получение объекта, поддерживаемого браузером (ActiveXObject - для IE6, IE5)
     var xhr = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
     // Отслеживание состояния завершенности запроса
     xhr.onreadystatechange = function () {
         // Если запрос завершен
         if (xhr.readyState === 4) {
-            var text = xhr.responseText;
             // Вызов функции обратного вызова при успешном завершении запроса
             if (xhr.status === 200) {
                 if (settings.success) {
-                    settings.success(text);
+                    settings.success(xhr.responseText);
                 }
                 return;
             }
@@ -77,12 +83,15 @@ function ajax(settings) {
             }
         }
     };
+
     // Установка параметров запроса
     xhr.open(settings.method, settings.url, settings.async);
-    // Установка заголовка POST-запроса
+
+    // Установка заголовка POST-запроса, если метод запроса - POST
     if (settings.method === "post") {
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     }
+
     // Открытие соединения и отправка запроса
     xhr.send(body);
 }
