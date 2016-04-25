cf0e09828ae0acfc79768b59241090e4c6ffae11
Ildar Ibragimov <iibragimov84@gmail.com>
2016-04-25 17:21:38 +0300

    Исправление бага: Описание: Не полностью раскрывается секция аккордеона Условие возникновения бага: когда у страницы нет вертикальной полосы прокрутки Выявленная причина: Значения высот содержимого секции передаются без учета того, что содержимое страницы при раскрытой секции аккордеона может выйти за пределы области просмотра окна браузера и тем самым вызвать появление вертикальной полосы прокрутки, которая уменьшит ширину блоков секции (и страницы вцелом), что в свою очередь прямо влияет на высоту сожержимого секции. Таким образом возникает несоответствие высот назначенных с фактическими

--- www/library/js/Plugins/plugin.accordion/accordion.js
+++ www/library/js/Plugins/plugin.accordion/accordion.js
@@ -12,90 +12,112 @@
  */
 function accordion(selector) {
     /**
-     * @property {boolean} [enable = false] - Cтатус доступности содержимого секции
-     * @public
-     */
-    this.enable = false;
-    /**
      * @property {object} self - Ссылка на текущий объект
+     * @private
      */
     var self = this;
     /**
      * Конструктор класса
      *
      * @constructor
-     * @param {array} element - Селектор элемента Аккордеон, Объект которого необходимо создать
+     * @param {array} selector - Селектор элемента Аккордеон, Объект которого необходимо создать
+     *
      * @return void
      */
     function constructor(selector) {
-        // Назначение обработчика события татча по заголовку секции аккордеона
-        selector.addEventListener("touchend", function(event) {
-            // Отмена действия по умолчанию браузера на событие
-            event.preventDefault();
-            // Обработка события, если оно было вызвано на элементе заголовка секции аккордеона
-            if (event.target.classList.contains("link")) {
-                // Вызов события "onclick"
-                event.target.click();                
-            }
-        });
+        var sectionsContents = selector.getElementsByClassName("accordion__section-content-wrapper");
+        // Вызов метода пересчета значений высот содержимых секций
+        resetSectionsHeights(sectionsContents);
         // Назначение обработчика события клика по заголовку секции аккордеона
         selector.addEventListener("click", function(event) {
             // Отмена действия по умолчанию браузера на событие
             event.preventDefault();
             // Обработка события, если оно было вызвано на элементе заголовка секции аккордеона
-            if (event.target.classList.contains("link")) {
+            if (event.target.classList.contains("link") || parent(event.target, ".link")) {
                 // Сворачивание/Разворачивание секции аккордеона в зависимости от ее текущего состояния
-                ( self.enable == true ) ? self.hide(event.target) : self.show(event.target);
+                self.toggle(event.target);
+            }
+        });
+        // Назначение обработчика события завершения анимации раскрытия секции аккордеона
+        selector.addEventListener('transitionend', function(event) {
+            // Условие на соответствие требованиям к отлавливаемому событию
+            if (event.propertyName == "height" && parent(event.target, '.accordion__section_open')) {
+                // Вызов метода пересчета значений высот содержимых секций
+                resetSectionsHeights(sectionsContents);
+                // Вызов метода назначение высоты элементу
+                setHeight(event.target, event.target.dataset.blockHeight+"px");
             }
         });
     }
     /**
-     * Метод разворачивает секцию
-     *
-     * @public
-     * @param {requestCallback} [callback = null]- Пользовательская функция обратного вызова
-     * @return void
-     *
-     * @description Добавляет DOM-узлам, переданным при инициализации класса в параметре "selector", дополнительные CSS-классы описания состояния секции "accordion__section_open" и вызывает функцию обратного вызова, если она была передана.
-     */
-    this.show = function(target, callback) {
-        toggle(target, "show", callback);
-    };
-    /**
-     * Метод сворачивает панель
-     *
-     * @public
-     * @param {requestCallback} [callback = null]- Пользовательская функция обратного вызова
-     * @return void
-     *
-     * @description Удаляет у DOM-узлов, переданных при инициализации класса в параметре "selector", дополнительные CSS-классы описания состояния секции "accordion__section_open" и вызывает функцию обратного вызова, если она была передана.
-     */
-    this.hide = function(target, callback) {
-        toggle(target, "hide", callback);
-    };
-    /**
-     * Метод Разворачивает/Сворачивает панель
-     *
+     * Метод пересчитывает значения высоты содержимого секции аккордеона
+     * 
+     * @method
      * @private
-     * @param {string} actionName - Действие, которое необходимо совершить с панелью
+     * @description пересчитывает значения высоты содержимого секции аккордеона и записывает полученное значение в атрибут "data-block-height" элемента с CSS-классом "accordion__section-content"
+     *
+     * @param {collection} sections - Коллекция DOM-узлов содержимого секций
+     */
+    function resetSectionsHeights(collection) {
+        var sectionParent, sectionStyles, i;
+        for (i = 0; i < collection.length; i++) {
+            sectionStyles = getComputedStyle(collection[i]);
+            sectionParent = parent(collection[i], ".accordion__section-content");
+            sectionParent.dataset.blockHeight = collection[i].offsetHeight + parseFloat(sectionStyles.marginTop) + parseFloat(sectionStyles.marginBottom);
+        }
+        sectionParent = sectionStyles = i = null;
+    }
+    /**
+     * Метод "setHeight"
+     *
+     * @method
+     * @private
+     * @description Изменяет значение высоты DOM-элемента
+     *
+     * @param {object} element - DOM-элемент, высота которого необходимо изменить
+     * @param {string} hightValue - Значение высоты для CSS-свойства "height"
+     *
+     * @return void
+     */
+    function setHeight(element, hightValue) {
+        if (getComputedStyle(element).height != hightValue) {
+            element.style.height = hightValue;
+        }
+    }
+    /**
+     * Метод Разворачивает/Сворачивает секцию
+     *
+     * @method
+     * @public
+     * @param {object} target - Объект, вызвавший событие
      * @param {requestCallback} [callback = null]- Пользовательская функция обратного вызова
      * @return void
      *
-     * @description Вспомогательный внутренний метод, выполняющих действия, которые ему были переданы
+     * @description Разворачивает секцию аккордеона, если она свернута и сворчивает, если развернута
      */
-    function toggle(target, actionName, callback) {
-        var section = parent(target, ".accordion__section")
+    this.toggle = function(target, callback) {
+        var section = parent(target, ".accordion__section");
         if (section) {
             callback = callback || null;
-            var state = (actionName == "show") ? true : false;
-            // Переключение занчения атрибута class, выбранных эелементов
+            /**
+             * Объявление переменных:
+             *
+             * state {boolean} - Результат проверки на наличие DOM-узла с CSS-классом 'accordion__section_open'
+             * sectionContent {object} - Объект с классом "accordion__section-content"
+             */ 
+            var state = (section.classList.contains('accordion__section_open')) ? false : true,
+                sectionContent = section.querySelector('.accordion__section-content');
+            // Смена значений атрибута "height" содержимого секции аккордеона
+            var h = (getComputedStyle(sectionContent).height != "0px")? '' : sectionContent.dataset.blockHeight + 'px';
+            // Вызов метода назначение высоты элементу
+            setHeight(sectionContent, h);
+            // Переключение значения атрибута class, выбранных эелементов
             section.classList.toggle('accordion__section_open', state);
-            // Изменение статуса состояния панели навигации
-            self.enable = state;
             // Вызов пользовательской функции обратного вызова, если пользовательская функция задана
             if(callback){ callback(); }
         }
-    }
+        section = h = null;
+    };
     // Вызов конструктора объекта
     constructor(selector);
 };
