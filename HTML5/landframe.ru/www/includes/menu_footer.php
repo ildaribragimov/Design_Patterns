8116decd39720b763ffe03663b7c60ec974f8610
Ildar Ibragimov <iibragimov84@gmail.com>
2016-04-29 18:56:32 +0300

    Каркас сайта-одностраничника. Версия 1.0

---
+++ HTML5/landframe.ru/www/includes/menu_footer.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+ * Описание шаблона вывода основного навигационного меню
+ */
+?>
+<nav class="footer__menu footer__menu menu menu_horiz menu_centered menu_footer">
+	<div class="menu__wrapper">
+        <div class="menu__content">
+            <ul class="menu__items">
+                <li class="menu__item">
+                    <a class="link link_inverted menu__link" href="#about-confidentiality" target="_self">Рекомендации по конфиденциальности</a>
+                </li>
+                <li class="menu__item">
+                    <a class="link link_inverted menu__link" href="#faq" target="_self">FAQ</a>
+                </li>
+            </ul>
+        </div>
+	</div>
+</nav>
